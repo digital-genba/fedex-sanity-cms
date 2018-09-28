a4291827c9baa8848fd969be480225e2f2d80a07
Amy O'Rourke <asorourke@agiledigital.com.au>
2018-09-28 14:31:33 +1000

    Logo plugin

--- schemas/artist.js
+++ schemas/artist.js
@@ -9,6 +9,18 @@ export default {
       type: 'string',
     },
     {
+      title: 'Image',
+      name: 'image',
+      type: 'image',
+      fields: [
+        {
+          title: 'Alt Text',
+          name: 'altText',
+          type: 'string',
+        }
+      ]
+    },
+    {
       title: 'Albums',
       name: 'albums',
       type: 'array',
